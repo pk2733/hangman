139cfe835d7be993535c550e7729947e89624fc0
pk2733 <84867963+pk2733@users.noreply.github.com>
2021-06-02 18:27:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -69,62 +69,62 @@ words = [line.strip().lower() for line in fin.readlines()]
 print("Welcome to Hangman game!")
 print('Type "exit" to exit the game!')
 
+def play_game():
 
-tries = 4
-hangman_screen(tries)        
+    tries = 4
+    hangman_screen(tries)
 
-
-while True:
-    word = random.choice(words)
-
-    length = len(word)
-   
-    print(f"Guess the word with {length} letters!")
-
-    word_list = []
-    
-    for i in range(length):
-        word_list.append('_')
-        
-    print(*word_list)
-
-    used_chars = []
-          
     while True:
-        print("\n")
-        letter = input("Guess a letter: ")
+        word = random.choice(words)
 
-        if letter in used_chars:
-            print("You have already guessed this letter")
-            continue
-        used_chars.append(letter)
-     
+        length = len(word)
+
+        print(f"Guess the word with {length} letters!")
+
+        word_list = []
+
         for i in range(length):
-            if letter == word[i]:
-                word_list[i] = letter     
-        if '_' not in word_list:
-            print("You won!")
-            print(f"word was", word)
-            break
-        elif 'exit' in letter:
-            sys.exit()
-  
-        if letter not in word:
-            if tries > 0:
-                print(hangman_screen(tries))
-                print(f"You have {tries} left")
-            elif tries == 0:
-                print(hangman_screen(tries))
-                print("You failed.")
-                again = str(input("Do you want to play again (type yes or no): "))
-                if again == "yes":
-                    print("Play again hangman")
-                elif again == "no":
-                    sys.exit()
-                break
-
-            tries -= 1
+            word_list.append('_')
 
         print(*word_list)
 
-      
+        used_chars = []
+
+        while True:
+            print("\n")
+            letter = input("Guess a letter: ")
+
+            if letter in used_chars:
+                print("You have already guessed this letter")
+                continue
+            used_chars.append(letter)
+
+            for i in range(length):
+                if letter == word[i]:
+                    word_list[i] = letter
+            if '_' not in word_list:
+                print("You won!")
+                print(f"word was", word)
+                break
+            elif 'exit' in letter:
+                sys.exit()
+
+            if letter not in word:
+                if tries > 0:
+                    print(hangman_screen(tries))
+                    print(f"You have {tries} left")
+                    tries -= 1
+                elif tries == 0:
+                    print(hangman_screen(tries))
+                    print("You failed.")
+                    again = str(input("Do you want to play again (type yes or no): "))
+                    if again == "yes":
+                        play_game()
+                    else:
+                        sys.exit()
+                    break
+
+            print(*word_list)
+
+
+play_game()
